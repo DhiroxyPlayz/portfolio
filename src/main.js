823a53d25d8450aaeb7f4b238ea17cb032ad2688
DhiroxyPlayz <pdhiroxy@gmail.com>
2024-11-22 14:02:01 +1100

    SKILLS section complete

--- src/main.js
+++ src/main.js
@@ -10,3 +10,19 @@ span.addEventListener('mouseout', () => {
     content.style.visibility = 'hidden';
     content.style.opacity = '0';
 });
+
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for(const card of document.getElementsByClassName('card')){
+    card.onmousemove = e => handleOnMouseMove(e);
+}
